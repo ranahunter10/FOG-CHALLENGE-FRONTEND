0e12c52e18e59ac386d429bbc952b1f01dd77e48
Ashish Rana <129541702+ranahunter10@users.noreply.github.com>
2024-07-05 16:31:32 +0530

    Update ServerInfoPanel.jsx

--- src/ServerInfoPanel.jsx
+++ src/ServerInfoPanel.jsx
@@ -8,6 +8,26 @@ const ServerInfoPanel = () => {
 
  const [serverData,setServerData]=useState({});
 
+ setServerData(  "name": "#1 | NASA | Noobs Welcome | Conquest 40Hz",
+  "region": "CONQUEST LARGE - SIEGE OF SHANGAI - NORMAL - 40HZ",
+  "linkData": "server protected by The_K-50 AntiCheat. Vip !Rules, Questions, Request, Appeal, Visit us: www.epg.gg | Discord https://discord.gg/3r5NK4d",
+  "players": "63/64",
+  "ping": "47ms",
+  "tickrate": "40HZ",
+  "regioneurope": "DE",
+  "punkbaster": "ON",
+  "fairfight": "ON",
+  "password": "OF",
+  "preset": "normal",
+  "tickets": "400",
+  "minimap": "ON",
+  "vehicals": "ON",
+  "teambalance": "ON",
+  "regenerativehealth": "ON",
+  "bulletdamage": "25",
+  "playerhealth": "100"
+})
+
  useEffect(()=>{
   
    axios.get(`${baseurl}/gamedata`).then((result) => {
